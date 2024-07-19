3f65610c4f125d2d0b6a47ce76b0846bad4b9c6c
sourajit samanta <sourajitsamanta00@gmail.com>
2024-07-20 03:43:13 +0530

    hi

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDjnUOTqdQoaVlmCf2l_PREN5aAXztCOog",
+  authDomain: "abcd-93fe8.firebaseapp.com",
+  projectId: "abcd-93fe8",
+  storageBucket: "abcd-93fe8.appspot.com",
+  messagingSenderId: "374974428176",
+  appId: "1:374974428176:web:c45c55d4f93ac1d2b9c695"
 };
 
 
@@ -17,4 +17,3 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
-
