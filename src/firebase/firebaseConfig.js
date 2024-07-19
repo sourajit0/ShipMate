a0ff70bb96fbd79f155b7cb526af1fae4836b5a1
sourajit0 <164853152+sourajit0@users.noreply.github.com>
2024-07-20 01:39:16 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjnUOTqdQoaVlmCf2l_PREN5aAXztCOog",
-  authDomain: "abcd-93fe8.firebaseapp.com",
-  projectId: "abcd-93fe8",
-  storageBucket: "abcd-93fe8.appspot.com",
-  messagingSenderId: "374974428176",
-  appId: "1:374974428176:web:c45c55d4f93ac1d2b9c695"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
